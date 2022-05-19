a280bce104be44f3a1af6d523ffaedd34d43680c
JasKale <105822699+JasKale@users.noreply.github.com>
2022-05-19 16:03:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 #import pandas
-#import requests
+import requests
 import snowflake.connector
 #from utllib.error import URLError
 
@@ -27,7 +27,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 #streamlit.text(fruityvice_response.json()) # just writes the data to teh screen
 
